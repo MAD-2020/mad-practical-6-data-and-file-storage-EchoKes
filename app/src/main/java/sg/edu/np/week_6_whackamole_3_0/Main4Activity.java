e04043fe25c008716467b5f0df701d54f08834b3
Dong Yuan Kester Yeo <s10185261@connect.np.edu.sg>
2020-06-14 22:44:14 +0800

    Validated some areas and added difficulty for game levels.

--- app/src/main/java/sg/edu/np/week_6_whackamole_3_0/Main4Activity.java
+++ app/src/main/java/sg/edu/np/week_6_whackamole_3_0/Main4Activity.java
@@ -5,40 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
 import java.util.Random;
 
 public class Main4Activity extends AppCompatActivity {
 
-    /* Hint:
-        1. This creates the Whack-A-Mole layout and starts a countdown to ready the user
-        2. The game difficulty is based on the selected level
-        3. The levels are with the following difficulties.
-            a. Level 1 will have a new mole at each 10000ms.
-                - i.e. level 1 - 10000ms
-                       level 2 - 9000ms
-                       level 3 - 8000ms
-                       ...
-                       level 10 - 1000ms
-            b. Each level up will shorten the time to next mole by 100ms with level 10 as 1000 second per mole.
-            c. For level 1 ~ 5, there is only 1 mole.
-            d. For level 6 ~ 10, there are 2 moles.
-            e. Each location of the mole is randomised.
-        4. There is an option return to the login page.
-     */
     private static final String FILENAME = "Main4Activity.java";
     private static final String TAG = "Whack-A-Mole3.0!";
 
     TextView Score;
     int Count, level;
-    Button B1, B2, B3, B4, B5, B6, B7, B8, B9, backBtn;
+    Button B1, B2, B3, B4, B5, B6, B7, B8, B9, backBtn, mole1, mole2;
     Button[] advBList;
     long millisUntilFinished;
     long interval;
@@ -70,7 +51,7 @@ public class Main4Activity extends AppCompatActivity {
     }
 
     private void placeMoleTimer(){
-        MoleCountDown = new CountDownTimer(1000, 1000) {
+        MoleCountDown = new CountDownTimer(Long.MAX_VALUE, 11000-(level*1000)) {
             @Override
             public void onTick(long millisUntilFinished) {
                 setNewMole();
@@ -114,6 +95,7 @@ public class Main4Activity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     doCheck(b);
+                    setNewMole();
                 }
             });
         }
@@ -145,18 +127,38 @@ public class Main4Activity extends AppCompatActivity {
         }
     }
 
-    public void setNewMole()
+    public void setNewMole() //used zachary's code
     {
         Random ran = new Random();
         int randomLocation = ran.nextInt(9);
-        for (int i = 0; i<advBList.length; i++){
-            if(randomLocation==i){
-                advBList[i].setText("*");
+        int newRandomLocation;
+
+        mole1 = advBList[randomLocation];
+        mole1.setText("*");
+
+        if(level>=6){
+            do {
+                newRandomLocation = ran.nextInt(9);
             }
-            else{
-                advBList[i].setText("O");
+            while (randomLocation == newRandomLocation);
+            mole2 = advBList[newRandomLocation];
+            mole2.setText("*");
+        }
+
+        for(Button b:advBList){
+            if(b!=mole1 && b!=mole2){
+                b.setText("O");
             }
         }
+//        for (int i = 0; i<advBList.length; i++){
+//            if(randomLocation==i){
+//                advBList[i].setText("*");
+//            }
+//            else{
+//                advBList[i].setText("O");
+//            }
+//        }
+
     }
 
     private void updateUserScore()
@@ -165,9 +167,12 @@ public class Main4Activity extends AppCompatActivity {
         MoleCountDown.cancel();
         myCountDown.cancel();
 
-        MyDBHandler myDBHandler = new MyDBHandler(Main4Activity.this);
-        myDBHandler.deleteAccount(data.getMyUserName());
-        data.getScores().set(level-1,Count);
-        myDBHandler.addUser(data);
+        int prevCount = data.getScores().get(0);
+        if(Count>prevCount){
+            MyDBHandler myDBHandler = new MyDBHandler(Main4Activity.this);
+            myDBHandler.deleteAccount(data.getMyUserName());
+            data.getScores().set(level-1,Count);
+            myDBHandler.addUser(data);
+        }
     }
 }
